3f4c31e09137b7330ff444c6f433eaf0121c7fa6
Ankit9943 <asharma65382@gmail.com>
2025-02-24 20:21:24 +0530

    added responsive navbar

--- script.js
+++ script.js
@@ -118,3 +118,14 @@ document.querySelectorAll(".service .row").forEach((row) => {
     });
   });
 });
+
+let btn = document.querySelector(".bar");
+let div = document.querySelector(".show");
+
+btn.addEventListener("click", () => {
+  if (div.style.display === "none") {
+    div.style.display = "block";
+  } else {
+    div.style.display = "none";
+  }
+});
